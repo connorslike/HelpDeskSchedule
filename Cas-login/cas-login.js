da45015a88843ddd0b267d9b5da16c9f49b9d887
Connor Slike <slike@hawaii.edu>
2017-10-17 15:15:53 -1000

    Make case page

---
+++ Cas-login/cas-login.js
@@ -0,0 +1,29 @@
+
+Template.Cas_Login.events({
+  /**
+   * Handle the click on the logout link.
+   * @param event The click event.
+   * @returns {boolean} False.
+   */
+  'click .cas-logout': function casLogout(event) {
+    event.preventDefault();
+
+    return false;
+  },
+
+  /**
+   * Handle the click on the login link.
+   * @param event The click event.
+   * @returns {boolean} False.
+   */
+  'click .cas-login': function casLogin(event) {
+    event.preventDefault();
+    const callback = function loginCallback(error) {
+      if (error) {
+        console.log(error);
+      }
+    };
+
+    return false;
+  },
+});
